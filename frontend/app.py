b0b16487fffad591b1a35f64fdb35b6284327e76
vanzylkruger <vanzylk@primitive-brain.com>
2024-06-22 21:42:45 +0800

    integrate to postgresql and ml

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,49 @@
 from flask import Flask, render_template, request
 import requests, logging
+import json
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="wapolice",
+    user="postgres",
+    password="root!@3"
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    fintran = "Key Word Extraction"
-    return render_template('index.html',fintran=fintran)
+    trans_txt = '{"questions":["Recommended questions"]}'
+    trans_json = json.loads(trans_txt)
+    trans = trans_json["questions"]
+    etypes = "Summary of emergency"
+    actions = 'Recommended action'
+    transc = ""
+    return render_template('index.html',trans=trans, etypes=etypes, actions=actions, transc=transc)
 
 @app.route('/keyword',methods=('GET','POST'))
 def keyword():
     if request.method=='POST':
-        fintran = str(request.form.get("final_span"))
-        print(fintran)
-        # headers = {"content-type":"application/json","accept":"application/json" }
-        # r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":fintran},headers=headers)
-        # app.logger.info(r.text)
-        # return r.text
-        return render_template("index.html", fintran=fintran)
+        transc = str(request.form.get("final_span"))
+        emergency = str(request.form.get("emergency"))
+        botOutput = str(request.form.get("botOutput"))
+
+        headers = {"content-type":"application/json","accept":"application/json" }
+        r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":transc},headers=headers)
+        
+        json_object = json.loads(r.text)
+        trans = json_object["questions"]
+        etypes = json_object["Emergency Type"]
+        actions = 'Please refer to '+json_object["Actions"]
+
+        cur = conn.cursor()
+        cur.execute(
+        "INSERT INTO transcript (transcript, emergency_type, agent_comments, emergency_type_assist, actions) VALUES (%s, %s, %s, %s, %s)", 
+        (transc, str(emergency), str(botOutput), str(etypes), str(actions)))
+        conn.commit()
+
+        return render_template("index.html", trans=trans, etypes=etypes, actions=actions, transc=transc)
     
 if __name__ == '__main__':
     app.run(debug=True)
