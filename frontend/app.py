35f2c417390ed187da533ccfd4185562e2ac160a
MelDie <cyraneika@gmail.com>
2024-06-22 14:57:32 +0800

    cleanup

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests, logging
 
 app = Flask(__name__)
@@ -9,10 +9,14 @@ def index():
 
 @app.route('/keyword',methods=('GET','POST'))
 def keyword():
-    headers = {"content-type":"application/json","accept":"application/json" }
-    r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":"there's a fire in my house"},headers=headers)
-    app.logger.info(r.text)
-    return r.text
+    if request.method=='POST':
+        fintran = str(request.form.get("final_span"))
+        print(fintran)
+        headers = {"content-type":"application/json","accept":"application/json" }
+        r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":fintran},headers=headers)
+        app.logger.info(r.text)
+        return r.text
+    
 
 
 if __name__ == '__main__':
