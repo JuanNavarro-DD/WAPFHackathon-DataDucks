13adfafdd122871fece9ba8a1db2625ff55c9f4d
MattP-DD <matthew.peacock@datadivers.io>
2024-06-22 16:56:39 +0800

    First pass bedrock update - needs testing

--- keywords/main.py
+++ keywords/main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 from typing import List, Dict
-import json
-import pickle
+#import json
 from keybert.llm import TextGeneration
 from keybert import KeyLLM
 
@@ -9,6 +8,16 @@ from ctransformers import AutoModelForCausalLM
 from transformers import pipeline, AutoTokenizer
 
 from huggingface_hub import login
+
+import boto3
+import json
+from fastapi import FastAPI
+
+app = FastAPI()
+
+boto3.setup_default_session(profile_name='dd-sandbox-jp')
+bedrock = boto3.client('bedrock-runtime')
+
 
 HFToken = json.load(open("huggingFaceToken.json"))["token"]
 
@@ -119,6 +128,10 @@ def classify_emergency(keywords:list) -> str:
     """
     Pass the extracted keywords and classify the emergency type
     """
+    modelId = "mistral.mistral-7b-instruct-v0:2"
+
+    accept = "application/json"
+    contentType = "application/json"
     examplePrompt = """
         <s>[INST]
         you are an emergency call assistant, you need to classify the type of emergency based on a set of keywords.
@@ -142,8 +155,21 @@ def classify_emergency(keywords:list) -> str:
 
         """
     prompt = examplePrompt + questionPrompt
-    services = generator(prompt,do_sample=True, top_k=50, top_p=0.95, temperature=1.0, return_full_text=False)
-    return str(services[0]['generated_text'])
+    #services = generator(prompt,do_sample=True, top_k=50, top_p=0.95, temperature=1.0, return_full_text=False)
+    body = json.dumps({
+        "prompt": prompt,
+        "max_tokens": 100,
+        "top_p": 0.9,
+        "temperature": 1.0
+    })
+    response = bedrock.invoke_model(
+        modelId=modelId, 
+        body=body, 
+        accept=accept, 
+        contentType=contentType
+        )
+    #return str(services[0]['generated_text'])
+    return json.loads(response.get('body').read())["outputs"][0]["text"]
 
 
 
@@ -153,62 +179,66 @@ def return_questions(keyword:str) -> list:
     return a set of questions to ask
     '''
     #TODO: update question set.
-    questions = [{'q':'What is your emergency?', 'qtype': ['basic']},
-    {'q':'Where are you located right now?', 'qtype': ['basic']},
-    {'q':'Can you provide the address or nearest intersection?', 'qtype': ['basic']},
-    {'q':'Are you calling from a mobile phone or a landline?', 'qtype': ['basic']},
-    {'q':'How many people are involved or injured?', 'qtype': ['basic']},
-    {'q':'Is anyone in immediate danger?', 'qtype': ['basic']},
-    {'q':'Are there any weapons involved or present at the scene?', 'qtype': ['basic']},
-    {'q':'Is the situation still ongoing, or has it already occurred?', 'qtype': ['basic']},
-    {'q':'Is the person conscious and breathing?', 'qtype': ['basic']},
-    {'q':'Do you know CPR or any first aid that could be administered?', 'qtype': ['basic']},
-    {'q':'Is there anything else important responders should know?', 'qtype': ['basic']},
-    {'q':'What is your emergency?', 'qtype':['robbery']},
-    {'q':'Where is the location of the robbery?', 'qtype':['robbery']},
-    {'q':'Can you describe the suspects?', 'qtype':['robbery']},
-    {'q':'Do you know if the suspects are still at the scene?', 'qtype':['robbery']},
-    {'q':'Are there any injuries?', 'qtype':['robbery']},
-    {'q':'Did you see or hear anything else that might be relevant?', 'qtype':['robbery']},
-    {'q':'Is anyone else with you?', 'qtype':['robbery']},
-    {'q':'Are the suspects still armed?', 'qtype':['robbery']},
-    {'q':'Are you in a safe place now?', 'qtype':['robbery']},
-    {'q':'Have the suspects made any demands or threats?', 'qtype':['robbery']},
-    {'q':'Can you lock yourself in a secure room or area?', 'qtype':['robbery']},
-    {'q':'Are there security cameras in the area that might have captured the incident?', 'qtype':['robbery']},
-    {'q': 'What is your emergency?', 'qtype': ['suspicious person']},
-    {'q': 'Where is the location of the suspicious person?', 'qtype': ['suspicious person']},
-    {'q': 'Can you describe the suspicious person?', 'qtype': ['suspicious person']},
-    {'q': 'What is the person doing that makes them suspicious?', 'qtype': ['suspicious person']},
-    {'q': 'Is the person alone, or are there others with them?', 'qtype': ['suspicious person']},
-    {'q': 'Do you feel threatened or unsafe because of this person\'s behavior?', 'qtype': ['suspicious person']},
-    {'q': 'Has the person approached you or anyone else?', 'qtype': ['suspicious person']},
-    {'q': 'Do you know if the person is armed?', 'qtype': ['suspicious person']},
-    {'q': 'Have you seen the person before in the area?', 'qtype': ['suspicious person']},
-    {'q': 'Are there any specific actions or statements made by the person that concern you?', 'qtype': ['suspicious person']},
-    {'q': 'Have you observed the person entering or leaving any buildings or vehicles?', 'qtype': ['suspicious person']},
-    {'q': 'Are there any security cameras in the area that might have captured the person\'s activities?', 'qtype': ['suspicious person']},
-    {'q':'What is your emergency?', 'qtype':['domestic violence']},
-    {'q':'Are you in a safe place right now?', 'qtype':['domestic violence']},
-    {'q':'Is anyone injured?', 'qtype':['domestic violence']},
-    {'q':'Can you tell me what happened?', 'qtype':['domestic violence']},
-    {'q':'Who is the perpetrator?', 'qtype':['domestic violence']},
-    {'q':'Is the perpetrator still present?', 'qtype':['domestic violence']},
-    {'q':'Is there a weapon involved?', 'qtype':['domestic violence']},
-    {'q':'Have there been previous incidents of violence?', 'qtype':['domestic violence']},
-    {'q':'Are there children or other vulnerable individuals present?', 'qtype':['domestic violence']},
-    {'q':'Do you have a safe place to go if you need to leave?', 'qtype':['domestic violence']},
-    {'q':'Have you contacted any friends, family members, or support services for assistance?', 'qtype':['domestic violence']},
-    {'q':'Would you like to speak with a counselor or advocate for additional support?', 'qtype':['domestic violence']},
-    {'q':'Is there anything else you think is important for responders to know?', 'qtype':['domestic violence']}
-    ]
+    # questions = [{'q':'What is your emergency?', 'qtype': ['basic']},
+    # {'q':'Where are you located right now?', 'qtype': ['basic']},
+    # {'q':'Can you provide the address or nearest intersection?', 'qtype': ['basic']},
+    # {'q':'Are you calling from a mobile phone or a landline?', 'qtype': ['basic']},
+    # {'q':'How many people are involved or injured?', 'qtype': ['basic']},
+    # {'q':'Is anyone in immediate danger?', 'qtype': ['basic']},
+    # {'q':'Are there any weapons involved or present at the scene?', 'qtype': ['basic']},
+    # {'q':'Is the situation still ongoing, or has it already occurred?', 'qtype': ['basic']},
+    # {'q':'Is the person conscious and breathing?', 'qtype': ['basic']},
+    # {'q':'Do you know CPR or any first aid that could be administered?', 'qtype': ['basic']},
+    # {'q':'Is there anything else important responders should know?', 'qtype': ['basic']},
+    # {'q':'What is your emergency?', 'qtype':['robbery']},
+    # {'q':'Where is the location of the robbery?', 'qtype':['robbery']},
+    # {'q':'Can you describe the suspects?', 'qtype':['robbery']},
+    # {'q':'Do you know if the suspects are still at the scene?', 'qtype':['robbery']},
+    # {'q':'Are there any injuries?', 'qtype':['robbery']},
+    # {'q':'Did you see or hear anything else that might be relevant?', 'qtype':['robbery']},
+    # {'q':'Is anyone else with you?', 'qtype':['robbery']},
+    # {'q':'Are the suspects still armed?', 'qtype':['robbery']},
+    # {'q':'Are you in a safe place now?', 'qtype':['robbery']},
+    # {'q':'Have the suspects made any demands or threats?', 'qtype':['robbery']},
+    # {'q':'Can you lock yourself in a secure room or area?', 'qtype':['robbery']},
+    # {'q':'Are there security cameras in the area that might have captured the incident?', 'qtype':['robbery']},
+    # {'q': 'What is your emergency?', 'qtype': ['suspicious person']},
+    # {'q': 'Where is the location of the suspicious person?', 'qtype': ['suspicious person']},
+    # {'q': 'Can you describe the suspicious person?', 'qtype': ['suspicious person']},
+    # {'q': 'What is the person doing that makes them suspicious?', 'qtype': ['suspicious person']},
+    # {'q': 'Is the person alone, or are there others with them?', 'qtype': ['suspicious person']},
+    # {'q': 'Do you feel threatened or unsafe because of this person\'s behavior?', 'qtype': ['suspicious person']},
+    # {'q': 'Has the person approached you or anyone else?', 'qtype': ['suspicious person']},
+    # {'q': 'Do you know if the person is armed?', 'qtype': ['suspicious person']},
+    # {'q': 'Have you seen the person before in the area?', 'qtype': ['suspicious person']},
+    # {'q': 'Are there any specific actions or statements made by the person that concern you?', 'qtype': ['suspicious person']},
+    # {'q': 'Have you observed the person entering or leaving any buildings or vehicles?', 'qtype': ['suspicious person']},
+    # {'q': 'Are there any security cameras in the area that might have captured the person\'s activities?', 'qtype': ['suspicious person']},
+    # {'q':'What is your emergency?', 'qtype':['domestic violence']},
+    # {'q':'Are you in a safe place right now?', 'qtype':['domestic violence']},
+    # {'q':'Is anyone injured?', 'qtype':['domestic violence']},
+    # {'q':'Can you tell me what happened?', 'qtype':['domestic violence']},
+    # {'q':'Who is the perpetrator?', 'qtype':['domestic violence']},
+    # {'q':'Is the perpetrator still present?', 'qtype':['domestic violence']},
+    # {'q':'Is there a weapon involved?', 'qtype':['domestic violence']},
+    # {'q':'Have there been previous incidents of violence?', 'qtype':['domestic violence']},
+    # {'q':'Are there children or other vulnerable individuals present?', 'qtype':['domestic violence']},
+    # {'q':'Do you have a safe place to go if you need to leave?', 'qtype':['domestic violence']},
+    # {'q':'Have you contacted any friends, family members, or support services for assistance?', 'qtype':['domestic violence']},
+    # {'q':'Would you like to speak with a counselor or advocate for additional support?', 'qtype':['domestic violence']},
+    # {'q':'Is there anything else you think is important for responders to know?', 'qtype':['domestic violence']}
+    # ]
+    with open('questions-type.json', 'r') as infile:
+        jsondata = infile.read()
+
+    questions = json.loads(jsondata)
     to_send = []
-    for q in questions:
+    for q in questions['emergencies']:
         if keyword in q['qtype']:
             to_send.append(q['q'])
         
     if len(to_send) == 0:
-        for q in questions:
+        for q in questions['emergencies']:
             if "basic" in q['qtype']:
                 to_send.append(q['q'])
     return to_send    
@@ -219,6 +249,10 @@ def suggest_service(keywords:str) -> str:
     Pass keywords to know what services to call
     Purpose is to identify the services to call based on the keywords
     '''
+    modelId = "mistral.mistral-7b-instruct-v0:2"
+
+    accept = "application/json"
+    contentType = "application/json"
     examplePrompt = """
         <s>[INST]
         based on the following keyworkds: 
@@ -246,5 +280,19 @@ def suggest_service(keywords:str) -> str:
     prompt = examplePrompt + servicePrompt
     customizedPrompt = prompt.replace('[keywords]', keywords)
     #Pass a document as a list to the llm
-    services = generator(customizedPrompt,do_sample=True, top_k=50, top_p=0.95, temperature=1.0, return_full_text=False)
-    return services[0]['generated_text']
+    # services = generator(customizedPrompt,do_sample=True, top_k=50, top_p=0.95, temperature=1.0, return_full_text=False)
+    # return services[0]['generated_text']
+    body = json.dumps({
+        "prompt": customizedPrompt,
+        "max_tokens": 100,
+        "top_p": 0.9,
+        "temperature": 1.0
+    })
+
+    response = bedrock.invoke_model(
+        modelId=modelId, 
+        body=body, 
+        accept=accept, 
+        contentType=contentType
+        )
+    return json.loads(response.get('body').read())["outputs"][0]["text"]
