4dbe916d2ce62593f84fbf726bfdf5cfd5c6b3d5
vanzylkruger <vanzylk@primitive-brain.com>
2024-06-22 19:37:48 +0800

    updated front end

--- frontend/app.py
+++ frontend/app.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html')
+    fintran = "Key Word Extraction"
+    return render_template('index.html',fintran=fintran)
 
 @app.route('/keyword',methods=('GET','POST'))
 def keyword():
     if request.method=='POST':
         fintran = str(request.form.get("final_span"))
         print(fintran)
-        headers = {"content-type":"application/json","accept":"application/json" }
-        r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":fintran},headers=headers)
-        app.logger.info(r.text)
-        return r.text
+        # headers = {"content-type":"application/json","accept":"application/json" }
+        # r= requests.post('http://127.0.0.1:8000/extract',json={"transcript":fintran},headers=headers)
+        # app.logger.info(r.text)
+        # return r.text
+        return render_template("index.html", fintran=fintran)
     
-
-
 if __name__ == '__main__':
     app.run(debug=True)
 
